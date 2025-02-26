7d996873eedcd5644b3afc1b82a93cde3cfa461b
Daniel Byshkin <online@bskdany.com>
2025-02-26 16:55:55 -0500

    added more projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -12,9 +12,37 @@ export const projects = [
     githubUrl: 'https://github.com/bskdany/typemeup'
   },
   {
+    title: 'Hybrid Engine Control System',
+    description: 'The control system for the 2025 Launch Canada Hybrid Engine from CuInSpace',
+    mediaUrl: '/hysim.webm', 
+    githubUrl: 'https://github.com/CarletonURocketry/hysim'
+  },
+  {
+    title: 'Twitter Crypto Sniper',
+    description: 'Worlds fastest twitter bot that can see whenever anyone posts with 200ms latency',
+    mediaUrl: '/sold.webp', 
+  },
+  {
+    title: 'QNX Roomba - uOttaHack 7 2nd place',
+    description: 'A QNX controlled Roomba that follows the light like a motorized sunflower',
+    mediaUrl: '/roomba.webp', 
+    githubUrl: 'https://github.com/bskdany/uOttaHack7'
+  },
+  {
     title: 'Persistent Browser',
     description: 'A on-demand cloud browser that persists sessions to allow automation workflows',
     mediaUrl: '/persistent_browser.webp', 
     githubUrl: 'https://github.com/bskdany/PersistentBrowser'
+  },
+  {
+    title: 'Decentralized Torrent System',
+    description: 'A peer-to-peer file sharing torrent platform with autonomous peer discovery',
+    mediaUrl: '/torrent.webm', 
+    githubUrl: 'https://github.com/asterbot/hack-the-hill-p2p'
+  },
+  {
+    title: 'Resume Roast - McGillHacks 12',
+    description: 'A platform that publicly roasts your resume to a live audience',
+    mediaUrl: '/resume_roast.webm', 
   },
 ]
